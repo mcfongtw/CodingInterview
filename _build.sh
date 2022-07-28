3431e27dc214a0373f8a9b4d83ec2204a3a82050
Michael Fong <mcfong.open@gmail.com>
2022-07-27 21:53:50 -0700

    Move output of _book/ to docs/

--- _build.sh
+++ _build.sh
@@ -6,5 +6,5 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
-rm -rf book-output/*
-mv _book/* book-output/
+rm -rf docs/*
+mv _book/* docs/
