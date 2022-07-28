dc592c498ce6d8215e4343489247e92701d8163f
Michael Fong <mcfong.open@gmail.com>
2022-07-27 21:49:15 -0700

    Move output of _book/ to book-output/ as build process

--- _build.sh
+++ _build.sh
@@ -6,3 +6,5 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+rm -rf book-output/*
+mv _book/* book-output/
